2171361d51a6c03c59c00db44e6279905af8e618
Maxime St-Pierre <maxime.st-pierre.3@ens.etsmtl.ca>
2016-04-22 22:21:45 -0400

    Update thermocouple.py

--- Thermocouple/thermocouple.py
+++ Thermocouple/thermocouple.py
@@ -3,15 +3,16 @@ from bbio.libraries.MAX31855 import MAX31855
 
 SPI1.open()
 
-
 cs_pin = 0
-
 
 thermocouple = MAX31855(SPI1, cs_pin)
 
+#There is nothing to do here
 def setup():
 
   pass
 
+
 def loop():
+  temp = thermocouple.readTempC()
   
